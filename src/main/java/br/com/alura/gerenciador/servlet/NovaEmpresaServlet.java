68192749c2637d7c987a2af9f1753f6720c7493d
Léo Freitas <leonardo.eng3965@gmail.com>
2021-02-16 20:54:28 -0300

    first commit

---
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,37 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public NovaEmpresaServlet() {
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		System.out.println("Cadastrando nova empresa");
+		
+		String nomeEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Empresa " + nomeEmpresa + " Cadastrada</body></html>");
+	}
+
+}
